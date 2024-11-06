e5e047d466f1dcd81b22e7988f876777c0eff5c8
ana ☆ <159488368+anaholiveira@users.noreply.github.com>
2024-11-06 13:09:14 -0400

    Add files via upload

--- teste/exercicio02/script.js
+++ teste/exercicio02/script.js
@@ -1,8 +1,14 @@
-function miniaturas () {
-    querySelector('#imgAmpliada') }
+const miniaturas = document.querySelectorAll('.miniatura') 
+const imgAmpliada = document.querySelector('#imgAmpliada') 
 
-miniaturas.forEach(function (miniatura) {
+miniaturas.forEach(miniatura => {
     miniatura.addEventListener('click', () => {
+        miniaturas.forEach(miniatura => {
+            miniatura.style.opacity = '0.4'
+        })
+        miniatura.style.opacity = '1';
+        imgAmpliada.src = miniatura.src;
+        imgAmpliada.style.display = 'block';
     })
     
 });
